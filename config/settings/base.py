2f91b09932f05792ede98e8162004e7597955bdc
meoyoug <eodyd426@gmail.com>
2025-07-29 17:19:11 +0900

    pillow install, static, media 파일 debug=True 환경에서 로컬 파일로 서빙 하도록 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -160,7 +160,7 @@ CORS_ALLOW_HEADERS = [
 # drf 관련 설정
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticatedOrReadOnly",),
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
