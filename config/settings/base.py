072d6986ecc33c6659e368b7ba6aa56a943001c8
HyunJu Lee <lhj7826@naver.com>
2025-10-15 18:07:42 +0900

    fix(base): local app 선언 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,7 +32,9 @@ THIRD_PARTY_APPS = [
     "django_filters",
 ]
 
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS
+LOCAL_APPS = ["apps.core", "apps.users"]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
