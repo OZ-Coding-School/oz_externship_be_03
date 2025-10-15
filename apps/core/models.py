6d02431e1c6767e97906562837a5f3ff1df76a5c
zzinypark <zzinypark21@gamil.com>
2025-10-15 17:53:32 +0900

    fix/코멘트반영

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.db import models
+from apps.core.utils.base62 import Base62
 
 
 class BaseModel(models.Model):
@@ -7,3 +9,14 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class UUIDBaseModel(BaseModel):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, null=False)
+
+    class Meta:
+        abstract = True
+
+    def get_short_uuid(self, length: int = 6) -> str:
+        """UUID를 Base62로 인코딩한 짧은 문자열 반환"""
+        return Base62.uuid_encode(self.uuid, length)
