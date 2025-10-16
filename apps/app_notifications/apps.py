f3368f45f57c629180ab3b192b658cb275f6a52b
lsh9511 <iamshopsh@naver.com>
2025-10-16 13:05:52 +0900

    🚚 build    : views.py 삭제,models.py models디렉토리 이동 및 설정코드 추가 , 마이그레이션

--- apps/app_notifications/apps.py
+++ apps/app_notifications/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppNotificationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "app_notifications"
+    name = "apps.app_notifications"
