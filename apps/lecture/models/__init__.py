813bbc31acd3e1d0a3ee6e73b5f10cb5becc9819
zzinypark <zzinypark21@gamil.com>
2025-10-16 10:45:13 +0900

    ✨ Lecture_models 작성

--- apps/lecture/models/__init__.py
+++ apps/lecture/models/__init__.py
@@ -1,5 +1,18 @@
-from .lecture import CrawledLecture
+from .bookmark import LectureBookmark
+from .category import Category, LectureCategory, UserPreferCategory
+from .lecture import CrawledLecture, DifficultyEnum, PlatformEnum
+from .review import CrawledLectureReview, RatingEnum
+from .search_log import LectureSearchLog
 
 __all__ = [
     "CrawledLecture",
+    "DifficultyEnum",
+    "PlatformEnum",
+    "Category",
+    "LectureCategory",
+    "UserPreferCategory",
+    "CrawledLectureReview",
+    "RatingEnum",
+    "LectureBookmark",
+    "LectureSearchLog",
 ]
