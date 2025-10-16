8e1f5db054f1e0388de1818c46f1fb804be97820
lsh9511 <iamshopsh@naver.com>
2025-10-16 14:16:18 +0900

    🚚 build    : NotificationType 승인/거절 알림 타입 나눔

--- apps/app_notifications/models/notifications_models.py
+++ apps/app_notifications/models/notifications_models.py
@@ -10,7 +10,8 @@ class Notification(BaseModel):
 
     class NotificationType(models.TextChoices):
         APPLICATION_CREATED = "APPLICATIONS_CREATED", "공고 지원 알림"
-        APPLICATION_STATUS_CHANGED = "APPLICATION_STATUS_CHANGED", "지원 승인/거절 알림"
+        APPLICATION_STATUS_APPROVAL = "APPLICATION_STATUS_APPROVAL", "지원 승인 알림"
+        APPLICATION_STATUS_REJECTION = "APPLICATION_STATUS_REJECTION", "지원 거절 알림"
         STUDY_MEMBER_JOINED = "STUDY_MEMBER_JOINED", "스터디 그룹 신규 참여 알림"
         STUDY_REVIEW_REQUEST = "STUDY_REVIEW_REQUEST", "스터디 후기 작성 요청 알림"
         STUDY_SCHEDULE_UPCOMING = "STUDY_SCHEDULE_UPCOMING", "예정 스케줄 알림"
