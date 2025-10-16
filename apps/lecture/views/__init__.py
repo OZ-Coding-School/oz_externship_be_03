9f0137e1b4b00b588789b8f0c154e31005a90ee9
zzinypark <zzinypark21@gamil.com>
2025-10-16 18:30:37 +0900

    feature/lecture_views

---
+++ apps/lecture/views/__init__.py
@@ -0,0 +1,11 @@
+from .lecture_views import (
+    LectureDetailView,
+    LectureListView,
+    LectureReviewListView,
+)
+
+__all__ = [
+    "LectureListView",
+    "LectureDetailView",
+    "LectureReviewListView",
+]
