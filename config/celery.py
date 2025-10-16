35ce3930da9a10165ccdd3326207913ac53ae53b
lsh9511 <iamshopsh@naver.com>
2025-10-16 19:26:05 +0900

    ✨ feat     : celery 패키지 설치, celery.py 추가 및 모듈 생성 코드 추가
    # Conflicts:
    #	poetry.lock
    #	pyproject.toml

---
+++ config/celery.py
@@ -0,0 +1,20 @@
+import os
+
+from celery import Celery
+from celery.app.task import Task
+
+# Set the default django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+app = Celery("config")
+
+# namespace = 접두사 역할(모든 설정에 CELERY가 붙어야 작동한다.)
+# CELERY_BROKEN_URL -> broken_url
+app.config_from_object("django.conf:settings", namespace="CELERY")
+# tasks.py를 스캔
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self: Task) -> None :
+    print(f"Request: {self.request!r}")
