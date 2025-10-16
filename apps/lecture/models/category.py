46598a45d0beef3bf070c29152f8b078348db111
zzinypark <zzinypark21@gamil.com>
2025-10-16 10:45:13 +0900

    ✨ Lecture_models 작성

---
+++ apps/lecture/models/category.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.db import models
+
+from apps.core.models import BaseModel
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=255, unique=True, null=False)
+
+    class Meta:
+        db_table = "categories"
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class LectureCategory(BaseModel):
+    lecture = models.ForeignKey(
+        "lecture.CrawledLecture", on_delete=models.CASCADE, null=False, related_name="lecture_categories"
+    )
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, null=False, related_name="lecture_categories")
+
+    class Meta:
+        db_table = "lecture_categories"
+        unique_together = [["lecture", "category"]]
+
+    def __str__(self) -> str:
+        return f"{self.lecture.title} - {self.category.name}"
+
+
+class UserPreferCategory(BaseModel):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, related_name="prefer_categories"
+    )
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, null=False, related_name="preferred_by_users")
+
+    class Meta:
+        db_table = "user_prefer_categories"
+        unique_together = [["user", "category"]]
+
+    def __str__(self) -> str:
+        return f"{self.user.nickname} - {self.category.name}"
