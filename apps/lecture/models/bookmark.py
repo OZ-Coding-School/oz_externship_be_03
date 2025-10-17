955f017257d68e634e3affbba28fe0720fabe7c6
zzinypark <zzinypark21@gamil.com>
2025-10-17 11:25:41 +0900

    fix/mypy수정

--- apps/lecture/models/bookmark.py
+++ apps/lecture/models/bookmark.py
@@ -17,4 +17,4 @@ class LectureBookmark(BaseModel):
         unique_together = (("user", "lecture"),)
 
     def __str__(self) -> str:
-        return f"{self.user.nickname} - {self.lecture.title}"  # type:ignore # TODO:user파트 머지 후 주석삭제
+        return f"{self.user.nickname} - {self.lecture.title}"
