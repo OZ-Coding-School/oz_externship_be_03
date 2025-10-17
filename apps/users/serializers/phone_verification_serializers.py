1c73845f1d18bc26ae686d8eadb4e0f1f2414b76
fullrestic <restinplastic@naver.com>
2025-10-17 17:39:49 +0900

    💡 chore    : 휴대폰 번호, 닉네임 공통 검증기 사용해 검증하도록 수정

--- apps/users/serializers/phone_verification_serializers.py
+++ apps/users/serializers/phone_verification_serializers.py
@@ -1,10 +1,11 @@
-# apps/users/serializers/phone_verification_serializers.py
 from __future__ import annotations
 
 from typing import Any, Dict, Literal
 
 from django.core.validators import RegexValidator
 from rest_framework import serializers
+
+from apps.users.validators import validate_korean_phone
 
 PhonePurpose = Literal["signup", "find_email", "change_phone"]
 PURPOSE_CHOICES: tuple[tuple[str, str], ...] = (
@@ -13,19 +14,13 @@ PURPOSE_CHOICES: tuple[tuple[str, str], ...] = (
     ("change_phone", "휴대폰 변경"),
 )
 
-# 휴대폰 형식 검증 TODO : 공통으로 뺄 경우 그거 사용하기
-phone_validator = RegexValidator(
-    regex=r"^010\d{8}$",
-    message="휴대폰 번호 형식이 올바르지 않습니다. 예) 01012345678",
-)
-
 
 class PhoneVerificationSendCodeSerializer(serializers.Serializer[Dict[str, Any]]):
     """
     휴대폰 인증코드 전송 요청
     """
 
-    phone_number = serializers.CharField(write_only=True, validators=[phone_validator])
+    phone_number = serializers.CharField(write_only=True, validators=[validate_korean_phone])
     purpose = serializers.ChoiceField(choices=PURPOSE_CHOICES, write_only=True)
 
 
@@ -34,7 +29,7 @@ class PhoneVerificationConfirmCodeSerializer(serializers.Serializer[Dict[str, An
     휴대폰 인증코드 확인
     """
 
-    phone_number = serializers.CharField(write_only=True, validators=[phone_validator])
+    phone_number = serializers.CharField(write_only=True, validators=[validate_korean_phone])
     purpose = serializers.ChoiceField(choices=PURPOSE_CHOICES, write_only=True)
     code = serializers.CharField(write_only=True, min_length=6, max_length=6)
 
