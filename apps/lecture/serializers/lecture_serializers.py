b661406f48d2f42b4b98f1fb5a6528765622562f
zzinypark <zzinypark21@gamil.com>
2025-10-17 18:18:47 +0900

    ✨ Lecture-lecture

--- apps/lecture/serializers/lecture_serializers.py
+++ apps/lecture/serializers/lecture_serializers.py
@@ -40,34 +40,3 @@ class LectureListSerializer(serializers.ModelSerializer[CrawledLecture]):
         categories = Category.objects.filter(lecture_categories__lecture=obj).distinct()
         return cast(list[dict[str, Any]], CategorySerializer(categories, many=True).data)
 
-
-class LectureDetailSerializer(serializers.ModelSerializer[CrawledLecture]):
-    """강의 상세 조회용 Serializer"""
-
-    categories = serializers.SerializerMethodField()
-
-    class Meta:
-        model = CrawledLecture
-        fields = [
-            "id",
-            "uuid",
-            "title",
-            "instructor",
-            "thumbnail_img_url",
-            "description",
-            "categories",
-            "difficulty",
-            "duration",
-            "original_price",
-            "discount_price",
-            "platform",
-            "average_rating",
-            "url_link",
-            "created_at",
-            "updated_at",
-        ]
-
-    def get_categories(self, obj: CrawledLecture) -> list[dict[str, Any]]:
-        """강의에 속한 카테고리 목록 반환"""
-        categories = Category.objects.filter(lecture_categories__lecture=obj).distinct()
-        return cast(list[dict[str, Any]], CategorySerializer(categories, many=True).data)
---
+++ apps/lecture/urls/__init__.py
@@ -0,0 +1,5 @@
+from .lecture_urls import urlpatterns
+
+__all__ = [
+    "urlpatterns",
+]
