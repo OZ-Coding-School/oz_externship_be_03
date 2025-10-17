5876adced1caeb4309c08c1ecb8d632551a2028e
lsh9511 <iamshopsh@naver.com>
2025-10-17 13:56:51 +0900

    ✨ feat     : 함수별 매개변수 할당 및 타입힌팅

--- apps/notifications/tasks.py
+++ apps/notifications/tasks.py
@@ -1,35 +1,35 @@
 from celery import shared_task
 
 @shared_task(serializer='json')
-def create_recruitment_applicant_task():
+def create_recruitment_applicant_task(recruitment_id: int, applicant_id: int, receiver_id: int) -> str:
     ...
 
 @shared_task(serializer='json')
-def create_recruitment_application_approval_task():
+def create_recruitment_application_approval_task(recruitment_id: int, applicant_id: int, status: str)-> str:
     ...
 
 @shared_task(serializer='json')
-def create_recruitment_application_rejection_task():
+def create_recruitment_application_rejection_task(recruitment_id: int, applicant_id: int, status: str)-> str:
     ...
 
 @shared_task(serializer='json')
-def create_studygroup_join_task():
+def create_studygroup_join_task(study_group_id: int, user_id: int) -> str:
     ...
 
 @shared_task(serializer='json')
-def create_studygroup_reviewrequest_task():
+def create_studygroup_reviewrequest_task(study_group_id: int, group_members_id: int) -> str:
     ...
 
 @shared_task(serializer='json')
-def create_schedule_upcoming_task():
+def create_schedule_upcoming_task(study_group_id: int,group_schedules_id:int, group_members_id: int) -> str:
     ...
 
 @shared_task(serializer='json')
-def create_schedule_today_task():
+def create_schedule_today_task(study_group_id: int,group_schedules_id:int, group_members_id: int) -> str:
     ...
 
 @shared_task(serializer='json')
-def create_study_record_task():
+def create_study_record_task(user_id: int, study_notes_id: int) -> str:
     ...
 
 
