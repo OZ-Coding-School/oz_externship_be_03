da964b5a24dec57dbbe64d49bc64f8b5286ce3c5
HyunJu Lee <lhj7826@naver.com>
2025-10-17 17:57:06 +0900

    💡 chore(users) : 파일명 수정

--- apps/users/models/managers.py
+++ apps/users/models/managers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 
 if TYPE_CHECKING:
-    from .social_users import SocialUser
+    from .social_user import SocialUser
     from .user import User
 
 
