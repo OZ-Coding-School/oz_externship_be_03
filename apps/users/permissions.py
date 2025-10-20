93ffba24eebf94a7992240c8baf00863934a7873
HyunJu Lee <lhj7826@naver.com>
2025-10-20 15:13:09 +0900

    🐛 fix(users): permission redis 중복 설정 제거

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -2,26 +2,16 @@ from __future__ import annotations
 
 import os
 
-import redis
+from django.core.cache import cache
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
 from rest_framework.views import APIView
 
-# Redis 설정
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
-# REDIS DB는 0~15 사이의 정수로 번호 설정해 사용, 설정하지 않을시 기본값 0
-# REDIS_DB = int(os.getenv("REDIS_DB", "0"))
-# r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
-
-
-# ---------------------------------------------------------------------------
-# 📞 휴대폰 인증 퍼미션
-# ---------------------------------------------------------------------------
 class PhoneVerifiedPermission(BasePermission):
     """
+    휴대폰 인증 퍼미션
+
     특정 purpose(목적)에 대해 '휴대폰 인증'이 완료된 사용자만 접근 가능.
     ex) permission_classes = [IsAuthenticated, PhoneVerifiedPermission]
         purpose = "change_phone"
@@ -38,19 +28,17 @@ class PhoneVerifiedPermission(BasePermission):
             return False
 
         key = f"verify:phone:{user.id}:{purpose}"
-        if not redis_client.exists(key):
+        if not cache.get(key):
             return False
 
-        # 원타임 소모
-        redis_client.delete(key)
+        cache.delete(key)
         return True
 
 
-# ---------------------------------------------------------------------------
-# 📧 이메일 인증 퍼미션
-# ---------------------------------------------------------------------------
 class EmailVerifiedPermission(BasePermission):
     """
+    이메일 인증 퍼미션
+
     특정 purpose(목적)에 대해 '이메일 인증'이 완료된 사용자만 접근 가능.
     ex) permission_classes = [IsAuthenticated, EmailVerifiedPermission]
         purpose = "reset_password"
@@ -67,8 +55,8 @@ class EmailVerifiedPermission(BasePermission):
             return False
 
         key = f"verify:email:{user.id}:{purpose}"
-        if not redis_client.exists(key):
+        if not cache.get(key):
             return False
 
-        redis_client.delete(key)
+        cache.delete(key)
         return True
