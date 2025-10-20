1115cdb2efd4035518e70fb4af19df6dabfd41c0
lsh9511 <iamshopsh@naver.com>
2025-10-20 11:37:32 +0900

    ✅ test     : tasks 테스트 코드 작성

--- apps/app_notifications/tests/test_tasks.py
+++ apps/app_notifications/tests/test_tasks.py
@@ -1,8 +1,33 @@
-from apps.app_notifications.tasks import create_recruitment_applicant_task
+from unittest.mock import patch
+from django.test import TestCase
 from apps.app_notifications import tasks
 
-def recruitment_create():
-    data = {"recruitmet_id" : 2,"reciever_id" :1}
+class CeleryTasksTest(TestCase):
 
-    tasks.create_recruitment_applicant_task.delay(data)
+    @patch('apps.app_notifications.tasks.create_recruitment_applicant_task.delay')
+    def test_create_recruitment_applicant_task_delay(self,mock_delay):
+        ''' 공고 지원 알림 task delay 테스트'''
+        #Given
+        recruitment_id = 1
+        receiver_id = 2
+        # When
+        tasks.create_recruitment_applicant_task.delay(recruitment_id, receiver_id)
+        # Then
+        mock_delay.assert_called_with(recruitment_id, receiver_id)
+
+    def  test_create_recruitment_applicant_task_execution(self):
+        ''' 공고 지원 알림 task 실행 테스트'''
+        #Given
+        recruitment_id = 1
+        receiver_id = 2
+
+        #When
+        result = tasks.create_recruitment_applicant_task(recruitment_id, receiver_id)
+
+        #Then
+        self.assertIsInstance(result, dict) # 타입 검사
+        self.assertEqual(result["user_id"],receiver_id) # 동등성 검사
+        self.assertEqual(result["type"],"APPLICATION_CREATED") # 동등성 검사
+        self.assertIn("새로운 지원자가 지원했습니다.",result["content"]) # 포함 되어있는지 확인
+
 
