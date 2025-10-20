7312d7553267077892c8a72180f01bcd51227b84
fullrestic <restinplastic@naver.com>
2025-10-20 17:36:38 +0900

    ✨ feat     : 휴대폰 인증 API URL 등록

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,7 @@ if settings.DEBUG:
             path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
             path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
             path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+
+            # Users app
+            path("api/v1/", include(("apps.users.urls", "users"), namespace="users")),
         ]
