ee0df2979749cb4ff3a008e373ffe38f0dfd016e
zzinypark <zzinypark21@gamil.com>
2025-10-20 13:48:31 +0900

    feature/lecture-serializers

--- apps/lecture/urls/lecture_urls.py
+++ apps/lecture/urls/lecture_urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 
-from apps.lecture.views import LectureListView, LectureReviewListView
-
 app_name = "lecture"
 
 urlpatterns = [
-    path("", LectureListView.as_view(), name="lecture-list"),
-    path("<int:lecture_id>/reviews", LectureReviewListView.as_view(), name="lecture-review-list"),
 ]
