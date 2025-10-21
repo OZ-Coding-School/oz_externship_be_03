dc0fc9e6c9c722f5a78a08be640df483ada5057f
fullrestic <restinplastic@naver.com>
2025-10-21 10:51:36 +0900

    💡 chore    : url 파일 이름 변경

--- apps/users/urls/__init__.py
+++ apps/users/urls/__init__.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 app_name = "users"
 
 urlpatterns = [
-    path("", include(("apps.users.urls.phone_verifications", "phone_verifications"))),
+    path("", include(("apps.users.urls.phone_verification_urls", "phone_verifications"))),
 ]
