237355cd5de5aa1547a49aad5efb64813540aab2
이채희 <dldial0902@gmail.com>
2025-10-21 15:08:38 +0900

    💡 chore    : pk 추가

--- apps/recruitments/models/bookmark.py
+++ apps/recruitments/models/bookmark.py
@@ -5,6 +5,7 @@ from .recruitments import Recruitment
 
 
 class Bookmark(models.Model):
+    pk = models.CompositePrimaryKey("user_id", "recruitment_id")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="bookmarks")
     recruitment = models.ForeignKey(Recruitment, on_delete=models.CASCADE, related_name="bookmarks")
     created_at = models.DateTimeField(auto_now_add=True)
