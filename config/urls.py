c490eb40628723d26096722c682495c61c3063c4
fullrestic <restinplastic@naver.com>
2025-10-21 18:11:11 +0900

    ♻️ refactor : include 대신 언패킹 방식으로 url 등록

--- config/urls.py
+++ config/urls.py
@@ -7,9 +7,11 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 
+from apps.users.urls import urlpatterns as users_urlpatterns
+
 urlpatterns: list[URLPattern | URLResolver] = [
     path("api/lectures/", include("apps.lecture.urls")),
-    path("api/v1/", include(("apps.users.urls", "users"), namespace="users")),
+    *users_urlpatterns,
 ]
 
 if settings.DEBUG:
