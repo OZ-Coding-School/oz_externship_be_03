c2ad10a3dcd97e3dadcd340540e938962b84210d
monda95 <jungmonda@naver.com>
2025-10-21 13:36:20 +0900

    ✨임시작성 폴더 제거
    - root 경로에 나와있던studies app 제거

--- studies/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("studies/notes/", views.StudyNoteListCreateView.as_view(), name="study_note_list_create"),
-    
-    path("studies/notes/<int:note_id>/", views.StudyNoteDetailView.as_view(), name="study_note_detail"),
-]
