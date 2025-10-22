37db25130d07c11162f5f73c5dadbd78a97c8ec5
HYUK741 <dnwlslzx12@gmail.com>
2025-10-22 15:08:23 +0900

    ✨ feat : 리뷰 생성 api 구현
    # Conflicts:
    #	apps/studies/urls.py

--- apps/studies/urls.py
+++ apps/studies/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from apps.studies.views.members import DelegateLeaderAPIView
 from apps.studies.views.reviews import ReviewCreateView
 from apps.studies.views.members import DelegateLeaderAPIView
 from apps.studies.views.reviews import ReviewCreateView
@@ -17,3 +18,4 @@ urlpatterns = [
         name="delegate-leader",
     ),
 ]
+
