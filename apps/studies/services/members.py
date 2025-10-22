4b5a1c5d657454655470ba935f1f174ff1f80821
ing2720 <dlguddns2@naver.com>
2025-10-22 10:45:01 +0900

    🐛 fix: 별도의 validate 함수로 분리
    # Conflicts:
    #	apps/studies/services/members.py

--- apps/studies/services/members.py
+++ apps/studies/services/members.py
@@ -3,19 +3,18 @@ from apps.studies.models.groups import StudyGroup
 
 # 스터디 그룹 리더 위임 비즈니스 로직
 class MemberService:
-    @staticmethod
-    def delegate_leader(group: StudyGroup, target_user_id: int) -> None:
-        MemberService.validate_study_group(study_group=group)
-        MemberService.validate_target_user_id(user_id=target_user_id)
-        # TODO: 추후 DB 업데이트 로직 추가
-        return
+    @classmethod
+    def delegate_leader(cls, group: StudyGroup, target_user_id: int) -> None:
+        cls.validate_study_group(study_group=group)
+        cls.validate_target_user_id(user_id=target_user_id)
+        # TODO: 실제 리더 위임 로직 추가
 
     @staticmethod
-    def validate_study_group(study_group: StudyGroup):
+    def validate_study_group(study_group: StudyGroup) -> None:
         if not isinstance(study_group, StudyGroup):
             raise ValueError("유효하지 않은 스터디 그룹입니다.")
-    
+
     @staticmethod
-    def validate_target_user_id(user_id: int):
+    def validate_target_user_id(user_id: int) -> None:
         if user_id <= 0:
             raise ValueError("유효하지 않은 유저 ID입니다.")
