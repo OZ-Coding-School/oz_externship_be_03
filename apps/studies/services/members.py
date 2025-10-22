727fa224236843bfe4fd686cd832a0d09996f9d2
ing2720 <dlguddns2@naver.com>
2025-10-22 10:38:30 +0900

    Update apps/studies/services/members.py
    Co-authored-by: 머용 <153697857+Meoyoug@users.noreply.github.com>

--- apps/studies/services/members.py
+++ apps/studies/services/members.py
@@ -5,9 +5,17 @@ from apps.studies.models.groups import StudyGroup
 class MemberService:
     @staticmethod
     def delegate_leader(group: StudyGroup, target_user_id: int) -> None:
-        if not isinstance(group, StudyGroup):
-            raise ValueError("유효하지 않은 스터디 그룹입니다.")
-        if target_user_id <= 0:
-            raise ValueError("유효하지 않은 유저 ID입니다.")
-        # 현재는 목데이터로 진행하나 추후 db업데이트 로직 추가
+        MemberService.validate_study_group(study_group=group)
+        MemberService.validate_target_user_id(user_id=target_user_id)
+        # TODO: 추후 DB 업데이트 로직 추가
         return
+
+    @staticmethod
+    def validate_study_group(study_group: StudyGroup):
+        if not isinstance(study_group, StudyGroup):
+            raise ValueError("유효하지 않은 스터디 그룹입니다.")
+    
+    @staticmethod
+    def validate_target_user_id(user_id: int):
+        if user_id <= 0:
+            raise ValueError("유효하지 않은 유저 ID입니다.")
