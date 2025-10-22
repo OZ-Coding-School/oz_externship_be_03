2a9760631ad71f58427afb3fa046265423d2ed9c
fullrestic <restinplastic@naver.com>
2025-10-22 13:00:00 +0900

    🎨 style    : 코드 포매팅

--- apps/users/urls/__init__.py
+++ apps/users/urls/__init__.py
@@ -1,9 +1,11 @@
-from django.urls import include, path, URLPattern, URLResolver
-from apps.users.urls.phone_verification_urls import urlpatterns as phone_verification_urls
+from django.urls import URLPattern, URLResolver, include, path
+
+from apps.users.urls.phone_verification_urls import (
+    urlpatterns as phone_verification_urls,
+)
 
 app_name = "users"
 
 urlpatterns: list[URLPattern | URLResolver] = [
     *phone_verification_urls,
 ]
-
