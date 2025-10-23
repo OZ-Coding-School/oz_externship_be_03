e981d68662a487c79df144bbde4871d0ad26a80e
Meoyoug <eodyd426@gmail.com>
2025-10-23 15:44:43 +0900

    🚚 build: 장고 Test 명령어 오버라이딩, CI 수정

---
+++ apps/core/management/commands/test.py
@@ -0,0 +1,14 @@
+import redis.client
+from django.conf import settings
+from django.core.management.commands.test import Command as TestCommand
+
+
+class Command(TestCommand):
+    def handle(self, *test_labels, **options):
+        self.teardown_redis()
+        super().handle(*test_labels, **options)
+        self.teardown_redis()
+
+    @staticmethod
+    def teardown_redis() -> None:
+        redis.client.Redis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/15").flushdb()
