696e50a3280f80598d5b89a8501439f171c44b1d
lee <a50639174@gmail.com>
2025-10-23 00:21:04 -0700

    ✨ feat     : 새로운 기능 추가 , recruitment_tag , tag 의 views,urls,serializers 추가 , 즉 , 태그 기능추가 , 특정공고 조회 및 삭제 기능추가

--- apps/recruitments/models/__init__.py
+++ apps/recruitments/models/__init__.py
@@ -6,3 +6,19 @@ from .recruitment_tag import RecruitmentTag
 from .recruitments import Recruitment
 from .search_log import SearchLog
 from .tag import Tag
+
+__all__ = [
+    "ApplicationStatus",
+    "RecruitmentAttachment",
+    "Bookmark",
+    "RecruitmentImage",
+    "RecruitmentTag",
+    "Recruitment",
+    "SearchLog",
+    "Tag",
+]
+
+
+def recruitment() -> None:
+    """중복된 함수 정의 제거 및 타입 명시."""
+    return None
