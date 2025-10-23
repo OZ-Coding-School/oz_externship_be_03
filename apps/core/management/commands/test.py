2672cdf25597b31fa0fa9e9c28343449783fddd3
Meoyoug <eodyd426@gmail.com>
2025-10-23 15:47:27 +0900

    add type annotations, run black, isort

--- apps/core/management/commands/test.py
+++ apps/core/management/commands/test.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 import redis.client
 from django.conf import settings
 from django.core.management.commands.test import Command as TestCommand
 
 
 class Command(TestCommand):
-    def handle(self, *test_labels, **options):
+    def handle(self, *test_labels: Any, **options: Any) -> None:
         self.teardown_redis()
         super().handle(*test_labels, **options)
         self.teardown_redis()
