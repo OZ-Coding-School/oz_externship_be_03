36ba923efca7161142aab4d2005550c8ba5c3b88
Meoyoug <eodyd426@gmail.com>
2025-10-23 15:44:43 +0900

    🚚 build: 장고 Test 명령어 오버라이딩, CI 수정

--- apps/core/utils/isolated_cache_testcase.py
+++ apps/core/utils/isolated_cache_testcase.py
@@ -8,7 +8,13 @@ from rest_framework.test import APITestCase
 
 
 class IsolatedRedisTestClient(APITestCase):
-    START_CACHE_INDEX = 5
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cache_settings = copy.deepcopy(settings.CACHES)
+        cache_settings["default"]["LOCATION"] = f"redis://{settings.REDIS_HOST}:6379/15"
+        cls._isolated_cache_settings_override = override_settings(CACHES=cache_settings)
+        cls._isolated_cache_settings_override.enable()
 
     def setUp(self) -> None:
         super().setUp()
@@ -16,21 +22,5 @@ class IsolatedRedisTestClient(APITestCase):
         # 각 테스트마다 고유한 UUID로 KEY_PREFIX를 설정
         self.CACHE_PREFIX = f"test_{uuid.uuid4().hex}_"
         cache_settings["default"]["KEY_PREFIX"] = self.CACHE_PREFIX
-        cache_settings["default"]["LOCATION"] = f"redis://{settings.REDIS_HOST}:6379/{self.get_redis_db_index()}"
         self._isolated_cache_settings_override = override_settings(CACHES=cache_settings)
         self._isolated_cache_settings_override.enable()
-        self.redis_client = get_redis_connection("default")
-
-    def tearDown(self) -> None:
-        self._isolated_cache_settings_override.disable()
-        self.redis_client.flushdb()  # 현재 DB만 초기화
-        super().tearDown()
-
-    def get_redis_db_index(self) -> int:
-        result = self.START_CACHE_INDEX
-        self.__class__.START_CACHE_INDEX += 1
-
-        if self.__class__.START_CACHE_INDEX > 15:
-            self.__class__.START_CACHE_INDEX = 5
-
-        return result
