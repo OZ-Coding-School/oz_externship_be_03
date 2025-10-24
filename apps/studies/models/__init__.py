539b09311fb9193e072882ef7d470dc8d230c000
monda95 <jungmonda@naver.com>
2025-10-24 10:45:50 +0900

    ✨ note모델 마이그레이션 및 그룹 모델 수정
    - 그룹 UUIDbaseModel 상속하게 수정
    - note model 마이그레이션 마이그레이트
    
    # Conflicts:
    #	apps/studies/models/groups.py
    
    # Conflicts:
    #	apps/studies/models/__init__.py

--- apps/studies/models/__init__.py
+++ apps/studies/models/__init__.py
@@ -1,3 +1,4 @@
 from .groups import StudyGroup
 from .reviews import Review
+from .notes import StudyNote
 from .schedules import GroupSchedule, ScheduleParticipant
--- apps/studies/models/notes.py
+++ apps/studies/models/notes.py
@@ -20,6 +20,8 @@ class StudyNote(BaseModel):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,  # 작성자 삭제되면 이 노트도 같이 삭제됨
         related_name="study_notes",  # user.study_notes 이런 식으로 접근 가능
+        null=True,  # mock 단계에서만 허용 서비스화에서 반드시 False로 변경
+        blank=True,
     )
 
     # 노트 기본 내용
