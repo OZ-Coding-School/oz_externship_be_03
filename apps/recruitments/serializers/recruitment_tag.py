39ed2938d9e0308b30b7c2a98f8c3d19b0076c31
lee <a50639174@gmail.com>
2025-10-24 00:50:13 -0700

    🎨 style    : 코정드 포매팅 수정  ✅ test     : 테스트 코드 추가/변경 (프로덕션 코드 변경 없음)

--- apps/recruitments/serializers/recruitment_tag.py
+++ apps/recruitments/serializers/recruitment_tag.py
@@ -1,12 +1,9 @@
+from typing import Any
+
 from rest_framework import serializers
 
-from apps.recruitments.models.recruitment_tag import RecruitmentTag
 
-
-class RecruitmentTagSerializer(serializers.ModelSerializer[RecruitmentTag]):
-    recruitment: serializers.StringRelatedField = serializers.StringRelatedField()  # type: ignore
-    tag: serializers.StringRelatedField = serializers.StringRelatedField()  # type: ignore
-
-    class Meta:
-        model = RecruitmentTag
-        fields = ["id", "recruitment", "tag", "created_at", "updated_at"]
+class RecruitmentTagMockSerializer(serializers.Serializer[Any]):
+    id = serializers.IntegerField()
+    recruitment = serializers.IntegerField()
+    tag = serializers.CharField()
