3845b7fcb52a543daf4ac343f4fbb87fa5108c4e
lee <a50639174@gmail.com>
2025-10-23 23:03:27 -0700

    ✅ test     : 테스트 코드 추가/변경 (프로덕션 코드 변경 없음) : recuitment_tat , 및 tag 의 테스트코드수정

--- apps/recruitments/urls/__init__.py
+++ apps/recruitments/urls/__init__.py
@@ -1,8 +1,5 @@
 from django.urls import include, path
 
-from .bookmark import urlpatterns as bookmark_urls
-from .recruitments import urlpatterns as recruitment_urls
-
 urlpatterns = [
     path("recruitments/", include(recruitment_urls)),
     path("bookmarks/", include(bookmark_urls)),
