eaf31355d4eab39760b4d72a8b1b66534dacef1e
lee <a50639174@gmail.com>
2025-10-27 00:15:25 -0700

    💡 chore    : 기능 추가 없이 코드 수정 (오타, 주석 등) : 피드백반영후코드수정

--- apps/recruitments/serializers/tag.py
+++ apps/recruitments/serializers/tag.py
@@ -3,8 +3,13 @@ from rest_framework import serializers
 from apps.recruitments.models import Tag
 
 
-class TagSerializer(serializers.ModelSerializer[Tag]):
-    """태그 모델 직렬화기"""
+class TagSerializer(serializers.ModelSerializer):
+    """태그 모델 직렬화기 (커스텀 에러 메시지 포함)"""
+
+    name = serializers.CharField(
+        required=True,
+        error_messages={"required": "태그 이름은 필수입니다."},
+    )
 
     class Meta:
         model = Tag
