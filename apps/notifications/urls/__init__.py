b1a75082f4415133f5bd0748d95238af8486dcd1
lsh9511 <iamshopsh@naver.com>
2025-10-27 15:00:12 +0900

    🎨 style : 코드 포매팅 수정

--- apps/notifications/urls/__init__.py
+++ apps/notifications/urls/__init__.py
@@ -1,7 +1,7 @@
 from django.urls import URLPattern, URLResolver
 
-from apps.notifications.urls.sse_urls import urlpatterns as sse_urlpatterns
 from apps.notifications.urls.list_urls import urlpatterns as list_urlpatterns
+from apps.notifications.urls.sse_urls import urlpatterns as sse_urlpatterns
 
 app_name = "notifications"
 
