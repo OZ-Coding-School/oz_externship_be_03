14bae68a0e4624308ebb687817eba6d9569928fe
Meoyoug <eodyd426@gmail.com>
2025-10-27 14:19:48 +0900

    💡 chore: url naming 중복제거

--- apps/recruitments/urls/bookmark.py
+++ apps/recruitments/urls/bookmark.py
@@ -6,7 +6,9 @@ from apps.recruitments.views.bookmark import (
 )
 
 urlpatterns = [
-    path("", BookmarkListCreateAPIView.as_view(), name="bookmark-list-create"),
+    path("", BookmarkListCreateAPIView.as_view(), name="recruitments-bookmark-list-create"),
     # uuid 기준으로 상세 조회/삭제
-    path("<uuid:bookmark_uuid>/", BookmarkRetrieveDestroyAPIView.as_view(), name="bookmark-detail-destroy"),
+    path(
+        "<uuid:bookmark_uuid>/", BookmarkRetrieveDestroyAPIView.as_view(), name="recruitments-bookmark-detail-destroy"
+    ),
 ]
