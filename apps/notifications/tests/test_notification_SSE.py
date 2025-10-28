dc0b2aace0aca62c3b98dd528649c27cb3f16a70
lsh9511 <iamshopsh@naver.com>
2025-10-29 00:38:08 +0900

    🎨 style    : 코드 포매팅 수정

--- apps/notifications/tests/test_notification_SSE.py
+++ apps/notifications/tests/test_notification_SSE.py
@@ -1,8 +1,8 @@
 import asyncio
+from datetime import date
 
 from django.contrib.auth import get_user_model
 from django.http import HttpRequest
-from datetime import date
 
 from apps.core.utils.isolated_cache_testcase import IsolatedRedisTestClient
 from apps.notifications.models import Notification
@@ -12,12 +12,14 @@ from apps.users.enums import Gender
 
 User = get_user_model()
 
+
 class TestSSEViews(IsolatedRedisTestClient):
 
     def setUp(self):
         super().setUp()
 
         from datetime import datetime
+
         self.user = User.objects.create(
             email="test@test.com",
             password="pass123",
@@ -34,16 +36,16 @@ class TestSSEViews(IsolatedRedisTestClient):
             nickname="test2",
             name="test2",
             phone_number="010-2222-1112",
-            birthday=date(1995,1,11),
+            birthday=date(1995, 1, 11),
             gender=Gender.MALE,
         )
 
         self.test_notification = {
-            "id":1,
-            "type":Notification.NotificationType.APPLICATION_CREATED,
-            "content":"테스트입니다.",
-            "created_at":datetime.now(),
-            "back_url_link":"test/"
+            "id": 1,
+            "type": Notification.NotificationType.APPLICATION_CREATED,
+            "content": "테스트입니다.",
+            "created_at": datetime.now(),
+            "back_url_link": "test/",
         }
 
     async def test_notification_stream(self):
@@ -51,47 +53,46 @@ class TestSSEViews(IsolatedRedisTestClient):
         request = HttpRequest()
         request.user = self.user
 
-        response = await notification_stream(request,self.user.id)
+        response = await notification_stream(request, self.user.id)
 
-        self.assertEqual(response.status_code,200)
-        self.assertEqual(response['Content-Type'],'text/event-stream')
-        self.assertEqual(response['Cache-Control'],'no-cache')
-        self.assertEqual(response['Connection'],'keep-alive')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response["Content-Type"], "text/event-stream")
+        self.assertEqual(response["Cache-Control"], "no-cache")
+        self.assertEqual(response["Connection"], "keep-alive")
 
         async def publish_notification():
             await asyncio.sleep(0.2)
-            await notification_pubsub.publish_notification(self.user.id,self.test_notification)
+            await notification_pubsub.publish_notification(self.user.id, self.test_notification)
 
         publish_task = asyncio.create_task(publish_notification())
 
         stream_content = []
         async for chunk in response.streaming_content:
-            stream_content.append(chunk.decode('utf-8'))
+            stream_content.append(chunk.decode("utf-8"))
             if len(stream_content) >= 2:
                 break
 
         await publish_task
 
-        self.assertIn('data:{"type": "connected"}',stream_content[0])
-        self.assertIn('"content": "테스트입니다."',stream_content[1])
-        self.assertIn('"type": "APPLICATIONS_CREATED"',stream_content[1])
+        self.assertIn('data:{"type": "connected"}', stream_content[0])
+        self.assertIn('"content": "테스트입니다."', stream_content[1])
+        self.assertIn('"type": "APPLICATIONS_CREATED"', stream_content[1])
 
     async def test_notification_stream_unauthenticated_user(self):
         """미인증 유저 테스트"""
         request = HttpRequest()
-        request.user = type('User',(),{'is_authenticated': False})()
+        request.user = type("User", (), {"is_authenticated": False})()
 
-        response = await notification_stream(request,self.user.id)
+        response = await notification_stream(request, self.user.id)
 
-        self.assertEqual(response.status_code,401)
-        self.assertEqual(response['Content-Type'],'text/event-stream')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response["Content-Type"], "text/event-stream")
 
     async def test_notificaiton_stream_wrong_user(self):
         request = HttpRequest()
         request.user = self.other_user
 
-        response = await notification_stream(request,self.user.id)
+        response = await notification_stream(request, self.user.id)
 
-        self.assertEqual(response.status_code,403)
-        self.assertEqual(response['Content-Type'],'text/event-stream')
-
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response["Content-Type"], "text/event-stream")
