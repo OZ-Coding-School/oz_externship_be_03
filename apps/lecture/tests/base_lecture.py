59610c9449bbd63adf89dddbc9063dfdb700143b
zzinypark <zzinypark21@gamil.com>
2025-10-29 18:29:56 +0900

    test:lecture-admin 테스트코드 추가

---
+++ apps/lecture/tests/base_lecture.py
@@ -0,0 +1,42 @@
+from rest_framework.test import APITestCase
+
+from apps.lecture.models import (
+    Category,
+    CrawledLecture,
+    LectureCategory,
+)
+
+
+
+class BaseLectureTest(APITestCase):
+    def setUp(self) -> None:
+        self.category1 = Category.objects.create(name="Python")
+        self.category2 = Category.objects.create(name="C++")
+
+        self.lecture1 = CrawledLecture.objects.create(
+            title="Python 기초",
+            instructor="홍길동",
+            average_rating=4.5,
+            duration=600,
+            difficulty="EASY",
+            description="Python 기초 강의",
+            platform="INFLEARN",
+            original_price=50000,
+            discount_price=30000,
+            url_link="https://www.inflearn.com/python",
+        )
+        LectureCategory.objects.create(lecture=self.lecture1, category=self.category1)
+
+        self.lecture2 = CrawledLecture.objects.create(
+            title="C++ 심화",
+            instructor="김철수",
+            average_rating=4.8,
+            duration=180,
+            difficulty="HARD",
+            description="C++ 심화 강의",
+            platform="INFLEARN",
+            original_price=80000,
+            discount_price=60000,
+            url_link="https://inflearn.com/C",
+        )
+        LectureCategory.objects.create(lecture=self.lecture2, category=self.category2)
--- apps/lecture/tests/test_lecture_list_api.py
+++ apps/lecture/tests/test_lecture_list_api.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-
+from .base_lecture import BaseLectureTest
 from apps.lecture.models import (
     Category,
     CrawledLecture,
@@ -17,8 +17,9 @@ from apps.lecture.models import (
 User = get_user_model()
 
 
-class LectureListApiViewTest(APITestCase):
+class LectureListApiViewTest(BaseLectureTest):
     def setUp(self) -> None:
+        super().setUp()
         self.list_url = reverse("lecture-list")
         self.user = User.objects.create_user(
             email="test@example.com",
@@ -30,36 +31,6 @@ class LectureListApiViewTest(APITestCase):
             gender="MALE",
         )
 
-        self.category1 = Category.objects.create(name="Python")
-        self.category2 = Category.objects.create(name="C++")
-
-        self.lecture1 = CrawledLecture.objects.create(
-            title="Python 기초",
-            instructor="홍길동",
-            average_rating=4.5,
-            duration=600,
-            difficulty="EASY",
-            description="Python 기초 강의",
-            platform="INFLEARN",
-            original_price=50000,
-            discount_price=30000,
-            url_link="https://www.inflearn.com/python",
-        )
-        LectureCategory.objects.create(lecture=self.lecture1, category=self.category1)
-
-        self.lecture2 = CrawledLecture.objects.create(
-            title="C++ 심화",
-            instructor="김철수",
-            average_rating=4.8,
-            duration=180,
-            difficulty="HARD",
-            description="C++ 심화 강의",
-            platform="INFLEARN",
-            original_price=80000,
-            discount_price=60000,
-            url_link="https://inflearn.com/C",
-        )
-        LectureCategory.objects.create(lecture=self.lecture2, category=self.category2)
 
     def test_lecture_list(self) -> None:
         """강의 목록 조회 성공"""
