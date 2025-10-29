ba243717f9bbc743ac22d8e55ebef216877c6f11
fullrestic <restinplastic@naver.com>
2025-10-29 16:09:23 +0900

    🐛 fix      : detail 설정 부분 & annotation 추가

--- apps/core/exceptions.py
+++ apps/core/exceptions.py
@@ -1,15 +1,19 @@
+from typing import Dict, Optional, Any
+
 from rest_framework import status
-from rest_framework.exceptions import APIException
-from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import APIException, _get_error_details
+
 
 # Conflict 409 Error Custom
 class Conflict(APIException):
     status_code = status.HTTP_409_CONFLICT
-    default_detail = _('Conflict data')
-    default_code = 'conflict'
+    default_detail = "중복된 데이터가 이미 존재합니다."
+    default_code = "conflict"
 
-    def __init__(self, detail=None, code=None):
+    def __init__(self, detail: Optional[str|Dict[str, Any]] = None, code: Optional[str] = None) -> None:
         if detail is None:
             detail = self.default_detail
         if code is None:
             code = self.default_code
+
+        self.detail = _get_error_details(detail, code)
