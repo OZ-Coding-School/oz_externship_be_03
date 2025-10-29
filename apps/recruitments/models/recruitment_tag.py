3022bb455bdd6df968cd601fc296b73b11f2ee33
Meoyoug <eodyd426@gmail.com>
2025-10-29 17:12:55 +0900

    🎨 style: run black, add type annotation

--- apps/recruitments/models/recruitment_tag.py
+++ apps/recruitments/models/recruitment_tag.py
@@ -5,6 +5,7 @@ from apps.core.models import BaseModel
 
 class RecruitmentTag(BaseModel):
     """공고-태그 연결 테이블"""
+
     pk = models.CompositePrimaryKey("recruitment_id", "tag_id")
     # 문자열 참조 사용 → 순환 참조 완전 차단
     recruitment = models.ForeignKey(
