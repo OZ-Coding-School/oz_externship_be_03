182c01984f91aada99021b854d77652d0ea6ea0c
fullrestic <restinplastic@naver.com>
2025-10-29 16:34:18 +0900

    ✨ feat     : User Manger에 휴대폰 번호 중복 체크 모듈 추가

--- apps/users/managers/managers.py
+++ apps/users/managers/managers.py
@@ -26,6 +26,15 @@ class UserManager(BaseUserManager["User"]):
             return self.get_active_user().filter(email=email).exists()
         return self.get_queryset().filter(email=email).exists()
 
+    def exists_phone(self, phone_number: str, check_active: Optional[bool] = False) -> bool:
+        """
+        휴대폰 번호 중복 확인
+        기본적으로 모든 유저를 대상으로 확인하지만, `check_active=True`이면 활성화된 유저만 확인함
+        """
+        if check_active:
+            return self.get_active_user().filter(phone_number=phone_number).exists()
+        return self.get_queryset().filter(phone_number=phone_number).exists()
+
     def exists_nickname(self, nickname: str, check_active: Optional[bool] = False) -> bool:
         """
         닉네임 중복 확인
