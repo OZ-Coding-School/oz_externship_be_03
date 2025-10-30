1bc726a3324ac33096da2d4358017f9d8129ab12
zzinypark <zzinypark21@gamil.com>
2025-10-30 15:02:48 +0900

    feat: category 조회 API 및 테스트코드 추가

--- apps/lecture/tests/test_category_list_api.py
+++ apps/lecture/tests/test_category_list_api.py
@@ -2,14 +2,32 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from apps.lecture.models import Category
+
 
 class CategoryListAPITest(APITestCase):
-    def setUp(self) -> None:
-        self.category_list_url = reverse("category-list")
+    category_list_url: str
+    category1: Category
+    category2: Category
+    category3: Category
+    category4: Category
 
-    def test_category_list_mock_data(self) -> None:
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.category_list_url = reverse("category-list")
+
+        cls.category1, cls.category2, cls.category3, cls.category4 = Category.objects.bulk_create(
+            [
+                Category(name="Python"),
+                Category(name="C++"),
+                Category(name="Django"),
+                Category(name="Spring"),
+            ]
+        )
+
+    def test_category_list_data(self) -> None:
+        """카테고리 조회 및 데이터 검증"""
         response = self.client.get(self.category_list_url)
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn("id", response.json()[0])
-        self.assertIn("name", response.json()[2])
+        self.assertEqual(len(response.data), 4)
+        self.assertEqual(response.data[3]["name"], "Spring")
