574e7697da128c49ad033608c3d0ccab2c9f7076
lsh9511 <iamshopsh@naver.com>
2025-10-31 22:56:05 +0900

    ✅ test     : 테스트코드로 잡힌 오류 수정

--- apps/notifications/views/SSE_views.py
+++ apps/notifications/views/SSE_views.py
@@ -1,5 +1,4 @@
 import json
-from pickle import FALSE
 from typing import AsyncGenerator
 
 from django.http import HttpRequest, StreamingHttpResponse
@@ -28,7 +27,7 @@ async def notification_stream(request: HttpRequest, user_id: int) -> StreamingHt
             yield f"data:{json.dumps({'type':'connected'}, ensure_ascii=False)}\n\n"
 
             # Redis 구독 처리
-            async for notification in notification_pubsub.subscribe_notification(user_id):
+            async for notification in notification_pubsub.subscribe_notification(user_id=user_id):
                 sse_data = json.dumps(notification, ensure_ascii=False)
                 yield f"data:{sse_data}\n\n"
 
