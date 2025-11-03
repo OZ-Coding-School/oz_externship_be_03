b712c1db0ab0e5ca72a4b6252ddbfdfea8649699
lsh9511 <iamshopsh@naver.com>
2025-11-03 15:09:27 +0900

    ✅ test: tasks 테스트코드 추가

--- apps/notifications/tests/test_tasks.py
+++ apps/notifications/tests/test_tasks.py
@@ -1,12 +1,13 @@
 import asyncio
-from datetime import date
+from datetime import date, timezone, datetime
 
 from django.contrib.auth import get_user_model
 
 from apps.core.utils.isolated_cache_testcase import IsolatedRedisTestClient
 from apps.notifications.models import Notification
 from apps.notifications.services.redis_pubsub_classify import notification_pubsub
-from apps.notifications.tasks import send_to_pubsub
+from apps.notifications.tasks import send_to_pubsub, send_study_group_notification
+from apps.studies.models import StudyGroup
 from apps.users.enums import Gender
 
 User = get_user_model()
@@ -33,7 +34,16 @@ class TasksTest(IsolatedRedisTestClient):
             back_url_link="https://example.com/test",
         )
 
+        self.study_group = StudyGroup.objects.create(
+            name="오즈코딩스쿨",
+            introduction="장고 익스턴십",
+            max_headcount=5,
+            start_at=datetime.now(timezone.utc),
+            end_at=datetime.now(timezone.utc),
+        )
+
     async def test_send_to_pubsub(self) -> None:
+        """to redis 알림 전송 테스트"""
         messages = []
 
         async def message_listener() -> None:
@@ -57,4 +67,31 @@ class TasksTest(IsolatedRedisTestClient):
         data = messages[0]
         self.assertEqual(data["id"], self.notification.id)
         self.assertEqual(data["type"], self.notification.type)
+        self.assertEqual(data["content"], self.notification.content)
+
+    async def test_send_to_pubsub_group(self)-> None:
+        """to redis 그룹 알림 전송 테스트"""
+        messages = []
+
+        async def group_message_listener() -> None:
+            async for message in notification_pubsub.subscribe_notification(group_ids=[str(self.study_group.id)]):
+                messages.append(message)
+                if len(messages) >= 1:
+                    break
+
+        listener_task = asyncio.create_task(group_message_listener())
+
+        await asyncio.sleep(0.1)
+
+        # 그룹 알림 전송
+        await send_study_group_notification(self.notification.id, self.study_group.id)
+
+        try:
+            await asyncio.wait_for(listener_task, timeout=5.0)
+        except asyncio.TimeoutError:
+            listener_task.cancel()
+
+        self.assertEqual(len(messages), 1)
+        data = messages[0]
+        self.assertEqual(data["id"], self.notification.id)
         self.assertEqual(data["content"], self.notification.content)
