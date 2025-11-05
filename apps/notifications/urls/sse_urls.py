4119cd757ac45b2a01d8322c691d8639023c576f
lsh9511 <iamshopsh@naver.com>
2025-11-05 14:54:57 +0900

    ♻️ refactor : urlpattern 변경

--- apps/notifications/urls/sse_urls.py
+++ apps/notifications/urls/sse_urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from apps.notifications.views.SSE_views import notification_stream
 
-urlpatterns = [path("sse/<int:user_id>/", notification_stream, name="sse_stream")]
+urlpatterns = [path("stream/", notification_stream, name="notification_stream")]
