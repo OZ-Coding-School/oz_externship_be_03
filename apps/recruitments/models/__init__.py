682548bca50315857b9ded30f29ae1961eb3135d
JuWon <ehdrbs8751@naver.com>
2025-11-05 13:16:03 +0900

    💡 chore    : 모델 추가

--- apps/recruitments/models/__init__.py
+++ apps/recruitments/models/__init__.py
@@ -4,7 +4,7 @@ from .bookmark import Bookmark
 from .recruitment_images import RecruitmentImage
 from .recruitment_tag import RecruitmentTag
 from .recruitments import Recruitment
-from .search_log import SearchLog
+from .search_log import RecruitmentSearchLog
 from .tag import Tag
 
 __all__ = [
@@ -15,6 +15,6 @@ __all__ = [
     "RecruitmentImage",
     "RecruitmentTag",
     "Recruitment",
-    "SearchLog",
+    "RecruitmentSearchLog",
     "Tag",
 ]
