2f80c478fe58b898ce5e61b3d767c93f0dd5bb8c
hoochucat <ohnanabanha@gmail.com>
2025-11-05 17:36:25 +0900

    chore: 마이파이 에러 수정

--- apps/users/urls/social_urls.py
+++ apps/users/urls/social_urls.py
@@ -5,7 +5,6 @@ from apps.users.views.social_auth_views import SocialAuthView
 app_name = "users"
 
 urlpatterns = [
-    path("auth/social/<str:provider>/", SocialAuthView.as_view(), name="social-login"),  # ✅ 공통 엔드포인트 추가
     path("auth/social/kakao/", SocialAuthView.as_view(), {"provider": "kakao"}, name="kakao-login"),
     path("auth/social/naver/", SocialAuthView.as_view(), {"provider": "naver"}, name="naver-login"),
 ]
