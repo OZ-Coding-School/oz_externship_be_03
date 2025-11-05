afc302172cf593a98ea786c771bb851b738d89d1
HyunJu Lee <lhj7826@naver.com>
2025-11-05 17:14:48 +0900

    💡 chore(users): admin url 식별용 주석 추가

--- apps/users/urls/admin_urls.py
+++ apps/users/urls/admin_urls.py
@@ -10,9 +10,15 @@ from apps.users.views.admin_withdrawal_views import AdminWithdrawalListView
 app_name = "admin"
 
 urlpatterns = [
+    # --------------------------------------------------------
+    # 어드민 유저 리스트 및 상세 조회, 수정, 삭제
+    # --------------------------------------------------------
     path("admin/users", AdminUserListView.as_view(), name="admin-user-list"),
     path("admin/users/<int:user_id>", AdminUserView.as_view(), name="admin-user-detail"),
     path("admin/users/<int:user_id>/update_role", AdminUserRoleUpdateView.as_view(), name="admin-user-role-update"),
+    # --------------------------------------------------------
+    # 어드민 탈퇴 유저 관리
+    # --------------------------------------------------------
     path(
         "admin/withdrawals",
         AdminWithdrawalListView.as_view(),
