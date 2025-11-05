2cd3b22624c9387d3b517840cf86dfd94910623b
rawonkim <krw3465@gmail.com>
2025-11-05 16:35:53 +0900

    🎨 style    : mypy 에러잡기

--- apps/lecture/management/commands/crawl_inflearn_review.py
+++ apps/lecture/management/commands/crawl_inflearn_review.py
@@ -13,35 +13,35 @@ from apps.lecture.models import CrawledLecture, CrawledLectureReview
 class Command(BaseCommand):
 
     def handle(self, *args: Any, **options: Any) -> None:
-        lectures = CrawledLecture.objects.filter(platform='INFLEARN')
+        lectures = CrawledLecture.objects.filter(platform="INFLEARN")
         course_id_to_lecture = {lecture.inflearn_course_id: lecture for lecture in lectures}
         course_ids = list(course_id_to_lecture.keys())
 
         if not course_ids:
-            self.stdout.write(self.style.WARNING('강의를 먼저 크롤링하세요!'))
+            self.stdout.write(self.style.WARNING("강의를 먼저 크롤링하세요!"))
             return
 
         crawler = InflearnReviewCrawlerAsync()
         db_data: List[Dict[str, Any]] = asyncio.run(
-            crawler.crawl_and_process_reviews(course_ids, max_concurrent_requests=15)
+            crawler.crawl_and_process_reviews(course_ids, max_concurrent_requests=15)  # type: ignore[arg-type]
         )
 
         with transaction.atomic():
             reviews = [
                 CrawledLectureReview(
-                    lecture=course_id_to_lecture[data['inflearn_course_id']],
-                    rating=data['rating'],
-                    content=data['content'],
-                    inflearn_review_id=data['inflearn_review_id']
+                    lecture=course_id_to_lecture[data["inflearn_course_id"]],
+                    rating=data["rating"],
+                    content=data["content"],
+                    inflearn_review_id=data["inflearn_review_id"],
                 )
                 for data in db_data
-                if data.get('inflearn_course_id') in course_id_to_lecture
+                if data.get("inflearn_course_id") in course_id_to_lecture
             ]
             CrawledLectureReview.objects.bulk_create(
                 reviews,
                 update_conflicts=True,
-                update_fields=['rating', 'content'],
-                unique_fields=['inflearn_review_id'],
+                update_fields=["rating", "content"],
+                unique_fields=["inflearn_review_id"],
             )
 
             self.stdout.write(self.style.SUCCESS(f"Successfully crawled {len(reviews)} reviews from INFLEARN."))
