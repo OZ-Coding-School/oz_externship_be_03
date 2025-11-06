cbc195ff9a2cae75697946f52257fffce658035a
hoochucat <ohnanabanha@gmail.com>
2025-11-06 10:55:10 +0900

    chore: rebase 후 url 누락으로 social_urls 추가

--- apps/users/urls/__init__.py
+++ apps/users/urls/__init__.py
@@ -3,6 +3,7 @@ from django.urls import URLPattern, URLResolver
 from apps.users.urls.admin_urls import urlpatterns as admin_urls
 from apps.users.urls.auth_urls import urlpatterns as auth_urls
 from apps.users.urls.user_urls import urlpatterns as users_urls
+from apps.users.urls.social_urls import urlpatterns as social_urls
 
 app_name = "users"
 
@@ -10,4 +11,5 @@ urlpatterns: list[URLPattern | URLResolver] = [
     *users_urls,
     *auth_urls,
     *admin_urls,
+    *social_urls
 ]
