ac1711f690e85111baa745e31cd76491bac9e209
JuWon <ehdrbs8751@naver.com>
2025-11-06 14:05:48 +0900

    💡 chore    : 마이그레이션 추가 💡 chore    : 마이그레이션 추가
    마이그레이션 파일 추가 및 search_log 파일 이름 변경

--- apps/recruitments/models/__init__.py
+++ apps/recruitments/models/__init__.py
@@ -4,7 +4,7 @@ from .bookmark import Bookmark
 from .recruitment_images import RecruitmentImage
 from .recruitment_tag import RecruitmentTag
 from .recruitments import Recruitment
-from .search_log import RecruitmentSearchLog
+from .recruitment_search_log import RecruitmentSearchLog
 from .tag import Tag
 
 __all__ = [
