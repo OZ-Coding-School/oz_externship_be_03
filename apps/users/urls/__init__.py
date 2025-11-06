8f27045977059ad2ece9dc296417389c177d5635
hoochucat <ohnanabanha@gmail.com>
2025-11-06 10:58:41 +0900

    chore: isort 정렬

--- apps/users/urls/__init__.py
+++ apps/users/urls/__init__.py
@@ -2,8 +2,8 @@ from django.urls import URLPattern, URLResolver
 
 from apps.users.urls.admin_urls import urlpatterns as admin_urls
 from apps.users.urls.auth_urls import urlpatterns as auth_urls
-from apps.users.urls.user_urls import urlpatterns as users_urls
 from apps.users.urls.social_urls import urlpatterns as social_urls
+from apps.users.urls.user_urls import urlpatterns as users_urls
 
 app_name = "users"
 
