254499f5c054d48d85cba377e0d0f177664d2bba
lsh9511 <iamshopsh@naver.com>
2025-11-06 14:38:00 +0900

    ✨ feat : 후기작성 알림 생성 signal 추가

--- apps/notifications/signals.py
+++ apps/notifications/signals.py
@@ -8,6 +8,8 @@ from django.dispatch import receiver
 from apps.notifications.models import Notification
 from apps.notifications.tasks import send_study_group_notification, send_to_pubsub
 from apps.recruitments.models.application import Application, ApplicationStatus
+from apps.studies.models import StudyGroup
+from apps.studies.models.groups import StudyGroupStatus, GroupMember
 
 logger = logging.getLogger(__name__)
 
@@ -72,3 +74,19 @@ def study_member_joined_created(sender: Any, instance: Application, created: boo
             )
 
             send_study_group_notification.delay(notification.id, str(study_group.id))
+
+@receiver(post_save, sender=StudyGroup)
+def study_group_review_created(sender:Any, instance: StudyGroup, created: bool, **kwargs: Any) -> None:
+    """스터디 그룹 종료시 후기 작성 알림"""
+    if not created and instance.status == StudyGroupStatus.ENDED:
+        # 그룹 멤버들 각각 개별 알림 생성( 알림 조회 API를 위해)
+        group_members = GroupMember.objects.filter(study_group=instance)
+
+        for member in group_members:
+            notification = Notification.objects.create(
+                user_id=member.user_id,
+                content=f"오늘은 {instance.name}의 종료일이에요! 스터디 후기를 기록해주세요!",
+                type=Notification.NotificationType.STUDY_REVIEW_REQUEST,
+                back_url_link=f"{settings.FRONTEND_DOMAIN}/api/v1/studies/groups/{instance.id}/reviews"
+            )
+            send_to_pubsub.delay(notification.id)
