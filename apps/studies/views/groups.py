072e9043c9042d7d8fea1196b5e1022e9e98ac38
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-11-06 09:56:01 +0900

    ✨ Feat: 스터디 그룹 목록 조회 API에 검색 로직 추가
    feat: 스터디 그룹 목록 조회 API에 검색 로직 추가

--- apps/studies/views/groups.py
+++ apps/studies/views/groups.py
@@ -58,8 +58,9 @@ class StudyGroupListCreateView(APIView):
         if status_param in StudyGroupStatus.values:
             queryset = queryset.filter(status=status_param)
 
-        if request.query_params.get("search", ""):
-            queryset = queryset.filter(name__icontains=request.query_params.get("search"))
+        search_param = request.query_params.get("search", "")
+        if search_param:
+            queryset = queryset.filter(name__icontains=search_param)
 
         queryset = queryset.annotate(current_headcount=Count("members"))
 
