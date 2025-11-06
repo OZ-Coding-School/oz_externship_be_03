e1d9f4cc1ea46a6d6126e9ab5aef2d981a15bad2
209512 <209512mon@gmail.com>
2025-11-06 14:48:03 +0900

    🐛 fix(lecture): recommendation url slash

--- apps/lecture/urls/recommendation_urls.py
+++ apps/lecture/urls/recommendation_urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.lecture.views.recommendation_views import RecommendationView
 
 urlpatterns = [
-    path("recommendations", RecommendationView.as_view(), name="recommendations"),
+    path("/recommendations", RecommendationView.as_view(), name="recommendations"),
 ]
