50348ed908f0a6d9c0a8539f0411a32b912854e9
lsh9511 <iamshopsh@naver.com>
2025-11-06 11:24:58 +0900

    💡 chore: 트레일링 슬래시 제거

--- apps/notifications/urls/sse_urls.py
+++ apps/notifications/urls/sse_urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from apps.notifications.views.SSE_views import notification_stream
 
-urlpatterns = [path("stream/", notification_stream, name="notification_stream")]
+urlpatterns = [path("stream", notification_stream, name="notification_stream")]
