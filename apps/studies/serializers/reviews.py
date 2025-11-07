07448c49da578d6bf22f866870c2fbe0dd038736
HYUK741 <dnwlslzx12@gmail.com>
2025-11-07 18:08:54 +0900

    💡 chore : ci테스트 오류 추가 작업 study_group 소스 제거

--- apps/studies/serializers/reviews.py
+++ apps/studies/serializers/reviews.py
@@ -96,7 +96,7 @@ class _AdminReviewAuthorSerializer(serializers.Serializer[Any]):
 
 class AdminReviewListSerializer(serializers.ModelSerializer[Review]):
 
-    study_group = _AdminReviewStudyGroupSerializer(source="study_group", read_only=True)
+    study_group = _AdminReviewStudyGroupSerializer(read_only=True)
     author = _AdminReviewAuthorSerializer(source="user", read_only=True)
     star_rating = StarRatingField(read_only=True, represent="int")
     created_at = serializers.DateTimeField(format=DATETIME_MINUTE_FMT, read_only=True)
