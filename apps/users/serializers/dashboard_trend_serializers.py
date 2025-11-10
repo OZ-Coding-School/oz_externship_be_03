6c6d336f936bd497235c791f1c530cf57bbe716b
fullrestic <restinplastic@naver.com>
2025-11-10 19:24:13 +0900

    ✨ feat     : 가입 추세 API 시리얼라이저 작성

--- apps/users/serializers/dashboard_trend_serializers.py
+++ apps/users/serializers/dashboard_trend_serializers.py
@@ -20,3 +20,15 @@ class WithdrawalTrendsDataSerializer(serializers.Serializer[Dict[str, Any]]):
     to_date = serializers.DateField()
     total_withdrawals = serializers.IntegerField()
     items = TrendItemSerializer(many=True)
+
+
+class SignupTrendsDataSerializer(serializers.Serializer[Dict[str, Any]]):
+    """
+    가입 추세 조회 응답 Serializer
+    """
+
+    interval = serializers.CharField()
+    from_date = serializers.DateField()
+    to_date = serializers.DateField()
+    total_signups = serializers.IntegerField()
+    items = TrendItemSerializer(many=True)
