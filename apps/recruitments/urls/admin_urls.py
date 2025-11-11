ea05f25b79e8117ec2dc1c334d1832903674b504
Meoyoug <eodyd426@gmail.com>
2025-11-11 19:52:45 +0900

    🐛 fix: application, recruitment api에 대해 uuid를 사용하도록 수정

--- apps/recruitments/urls/admin_urls.py
+++ apps/recruitments/urls/admin_urls.py
@@ -11,7 +11,11 @@ from apps.recruitments.views.admin_recruitment_views import (
 
 urlpatterns = [
     path("recruitments", AdminRecruitmentListAPIView.as_view(), name="admin-recruitment-list"),
-    path("recruitments/<int:recruitment_id>", AdminRecruitmentDetailAPIView.as_view(), name="admin-recruitment-detail"),
+    path(
+        "recruitments/<uuid:recruitment_uuid>", AdminRecruitmentDetailAPIView.as_view(), name="admin-recruitment-detail"
+    ),
     path("applications", ApplicationAdminAPIView.as_view(), name="admin-application-list"),
-    path("applications/<int:application_id>", ApplicationDetailAdminAPIView.as_view(), name="admin-application-detail"),
+    path(
+        "applications/<uuid:application_uuid>", ApplicationDetailAdminAPIView.as_view(), name="admin-application-detail"
+    ),
 ]
