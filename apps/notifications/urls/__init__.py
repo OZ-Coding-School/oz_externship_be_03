bfa0906589f73198eae93ac95ae7c25b6a7d0aff
SeongSu <kimsszzz4@naver.com>
2025-11-11 13:21:49 +0900

    💡 chore    : url 경로 수정

--- apps/notifications/urls/__init__.py
+++ apps/notifications/urls/__init__.py
@@ -2,10 +2,12 @@ from django.urls import URLPattern, URLResolver
 
 from apps.notifications.urls.list_urls import urlpatterns as list_urlpatterns
 from apps.notifications.urls.sse_urls import urlpatterns as sse_urlpatterns
+from apps.notifications.urls.read_urls import urlpatterns as read_urlpatterns
 
 app_name = "notifications"
 
 urlpatterns: list[URLPattern | URLResolver] = [
     *list_urlpatterns,
     *sse_urlpatterns,
+    *read_urlpatterns,
 ]
