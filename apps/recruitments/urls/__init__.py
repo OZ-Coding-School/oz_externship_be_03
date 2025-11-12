4564f16517034e82b1c97a875c54701240be2391
Meoyoug <eodyd426@gmail.com>
2025-11-12 19:02:59 +0900

    ✨ feat: application url 추가

--- apps/recruitments/urls/__init__.py
+++ apps/recruitments/urls/__init__.py
@@ -1,11 +1,10 @@
-from django.urls import include, path
-
 from apps.recruitments.urls.bookmark import urlpatterns as bookmark_urlpatterns
 from apps.recruitments.urls.recruitment_tags import (
     urlpatterns as recruitment_tags_urlpatterns,
 )
 from apps.recruitments.urls.recruitments import urlpatterns as recruitment_urlpatterns
 from apps.recruitments.urls.tags import urlpatterns as tags_urlpatterns
+from apps.recruitments.urls.applications import urlpatterns as application_urlpatterns
 
 app_name = "recruitments"
 
@@ -14,4 +13,5 @@ urlpatterns = [
     *bookmark_urlpatterns,
     *recruitment_tags_urlpatterns,
     *tags_urlpatterns,
+    *application_urlpatterns,
 ]
