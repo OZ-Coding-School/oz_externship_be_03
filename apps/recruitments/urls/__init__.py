1e4b6257cd35005e13656d4b3ce6a7b71add3502
Meoyoug <eodyd426@gmail.com>
2025-11-12 19:04:34 +0900

    🎨 style: run isort

--- apps/recruitments/urls/__init__.py
+++ apps/recruitments/urls/__init__.py
@@ -1,10 +1,10 @@
+from apps.recruitments.urls.applications import urlpatterns as application_urlpatterns
 from apps.recruitments.urls.bookmark import urlpatterns as bookmark_urlpatterns
 from apps.recruitments.urls.recruitment_tags import (
     urlpatterns as recruitment_tags_urlpatterns,
 )
 from apps.recruitments.urls.recruitments import urlpatterns as recruitment_urlpatterns
 from apps.recruitments.urls.tags import urlpatterns as tags_urlpatterns
-from apps.recruitments.urls.applications import urlpatterns as application_urlpatterns
 
 app_name = "recruitments"
 
