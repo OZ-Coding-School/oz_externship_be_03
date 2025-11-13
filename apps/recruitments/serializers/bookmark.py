8382c5d65eecd320b34db06ea3bf93c068921700
JuWon <ehdrbs8751@naver.com>
2025-11-13 13:12:18 +0900

    🐛 fix      : recruitments_id 로 받는것을 recruitments_uuid로 받게 수정
    recruitments_id 로 받는것을 recruitments_uuid로 받게 수정

--- apps/recruitments/serializers/bookmark.py
+++ apps/recruitments/serializers/bookmark.py
@@ -4,7 +4,6 @@ from rest_framework import serializers
 
 
 class BookmarkToggleSerializer(serializers.Serializer[Any]):
-    recruitment_id = serializers.IntegerField(read_only=True)
-    user_id = serializers.IntegerField(read_only=True)
+    recruitment_uuid = serializers.UUIDField(read_only=True)
     is_bookmarked = serializers.BooleanField(read_only=True)
     message = serializers.CharField(read_only=True)
