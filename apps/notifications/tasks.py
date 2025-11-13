9d4717717869cad663838a69786cfaf48f9742eb
lsh9511 <iamshopsh@naver.com>
2025-11-13 17:35:17 +0900

    🐛 fix : SSE 메시지 반환 안되는 오류 수정(shared_task 비동기 코드 실행이 안됨 -> 동기함수 안에 비동기함수로 실행), event loop가 닫히는 상황 발생 -> 매번 새로운 인스턴스 생성

--- apps/notifications/tasks.py
+++ apps/notifications/tasks.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 from datetime import date, timedelta
 
@@ -5,52 +6,66 @@ from celery import shared_task  # type: ignore
 from django.conf import settings
 
 from apps.notifications.models import Notification
-from apps.notifications.services.redis_pubsub_classify import notification_pubsub
+from apps.notifications.services.redis_pubsub_classify import notification_pubsub, RedisPubSubService
 from apps.studies.models.schedules import ScheduleParticipant
 
 logger = logging.getLogger(__name__)
 
 
 @shared_task  # type: ignore[misc]
-async def send_to_pubsub(notification_id: int) -> None:
-    try:
-        notification = await Notification.objects.select_related("user").aget(id=notification_id)
+def send_to_pubsub(notification_id: int) -> None:
 
-        await notification_pubsub.publish_notification(
-            user_id=notification.user.id,
-            notification_data={
+    async def _send_to_pubsub():
+
+        pubsub_service = RedisPubSubService() # 매번 새로운 인스턴스 생성
+
+        try:
+            notification = await Notification.objects.select_related("user").aget(id=notification_id)
+
+            await notification_pubsub.publish_notification(
+                user_id=notification.user.id,
+                notification_data={
+                    "id": notification.id,
+                    "type": notification.type,
+                    "content": notification.content,
+                    "back_url_link": notification.back_url_link,
+                    "created_at": notification.created_at.isoformat(),
+                    "is_read": notification.is_read,
+                },
+            )
+
+        except Exception as e:
+            logging.error(f"Redis pub 오류: {e}")
+
+    asyncio.run(_send_to_pubsub())
+
+
+@shared_task  # type: ignore[misc]
+def send_study_group_notification(notification_id: int, study_group_id: str) -> None:
+
+    async def _send_study_group_notification():
+
+        pubsub_service = RedisPubSubService() # 매번 새로운 인스턴스 생성
+
+        try:
+            notification = await Notification.objects.aget(id=notification_id)
+
+            notification_data = {
                 "id": notification.id,
                 "type": notification.type,
                 "content": notification.content,
                 "back_url_link": notification.back_url_link,
                 "created_at": notification.created_at.isoformat(),
                 "is_read": notification.is_read,
-            },
-        )
+            }
 
-    except Exception as e:
-        logging.error(f"Redis pub 오류: {e}")
+            await notification_pubsub.publish_group_notification(
+                group_id=study_group_id, notification_data=notification_data
+            )
+        except Exception as e:
+            logging.error(f"스터디 그룹 알림 발송 오류:{e}")
 
-
-@shared_task  # type: ignore[misc]
-async def send_study_group_notification(notification_id: int, study_group_id: str) -> None:
-    try:
-        notification = await Notification.objects.aget(id=notification_id)
-
-        notification_data = {
-            "id": notification.id,
-            "type": notification.type,
-            "content": notification.content,
-            "back_url_link": notification.back_url_link,
-            "created_at": notification.created_at.isoformat(),
-            "is_read": notification.is_read,
-        }
-
-        await notification_pubsub.publish_group_notification(
-            group_id=study_group_id, notification_data=notification_data
-        )
-    except Exception as e:
-        logging.error(f"스터디 그룹 알림 발송 오류:{e}")
+    asyncio.run(_send_study_group_notification())
 
 
 @shared_task(name="send_tomorrow_schedule_notifications")  # type: ignore[misc]
