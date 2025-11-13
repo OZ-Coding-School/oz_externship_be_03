7c0f52b2851ab473f55ad82a4871b8beae1570e0
BE12-OZ <dhwmzheld6470@gmail.com>
2025-11-13 14:25:59 +0900

    feat(chat): 웹소켓 연결에 JWT 인증 구현
    채팅 애플리케이션의 웹소켓 연결에 JWT 인증을 도입합니다.
    
    - 를 생성하여 웹소켓 스코프에 대한 JWT 토큰 유효성 검사 및 사용자 인증을 처리합니다.
    - 를 수정하여 새로운 를 웹소켓 프로토콜 라우터에 통합하고, 기본 을 대체합니다.
    -  및 에서 Django 앱 레지스트리가 완전히 초기화된 후에 Django 종속 모듈을 가져오도록 하여  오류를 해결했습니다.
    -  타입 검사 오류를 해결하기 위해 에 타입 힌트 및 명시적 캐스트를 추가했습니다.
    
    이 변경으로 웹소켓 연결이 쿼리 파라미터로 전달된 JWT 토큰을 사용하여 인증될 수 있도록 하여, 프로젝트의 기존 JWT 인증 체계와 일치시킵니다.

--- config/asgi.py
+++ config/asgi.py
@@ -1,6 +1,5 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -9,10 +8,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django_asgi_app = get_asgi_application()
 
 import apps.chat.routing
+from apps.chat.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter(apps.chat.routing.websocket_urlpatterns)),
+        "websocket": JWTAuthMiddleware(URLRouter(apps.chat.routing.websocket_urlpatterns)),
     }
 )
